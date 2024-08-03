0cb2f80e0442b6c71eff647debbe22a8f26af88b
Vladimir Popov <vladimirv2312@gmail.com>
2024-08-03 18:42:45 +0300

    stack

---
+++ shit.py
@@ -0,0 +1,24 @@
+class Stack:
+    list_value = []
+    size = 0
+
+    def __init__():
+        list_value = None
+
+    def push(self,value):
+        self.list_value[self.num] = value
+        size+=size
+        
+    def pop(self):
+        if size > 0:
+            self.list_value[self.num] = None
+            size-=size
+        else:
+            print("stack is empty")
+
+    def clear(self):
+        if self.size == 0:
+            print("stack is empty")
+        else:
+            self.list_value = None
+            print("stack is clear")
